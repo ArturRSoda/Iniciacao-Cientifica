6419803e85f8aaf19e9fb080d10a0c33189de32c
root <root@desktop-artur>
2023-09-08 13:27:19 -0300

    Binary_Search

---
+++ Binary_Search/binary-search.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+int binary_search(int n, int v[], int x) {
+	int k = 0;
+	for (int b = n/2; b >= 1; b /= 2) {
+		while ((b+k < n) && (v[k+b] <= x)) {
+			k += b;
+		}
+	}
+	if (v[k] == x) return k;
+	else return -1;
+}
+
+
+int main() {
+
+	int v[] = {3, 5, 6, 1, 5};
+
+	int b;
+	b = binary_search(5, v, 3);
+	printf("%d \n", b);
+
+
+
+	
+
+	return 0;
+}
