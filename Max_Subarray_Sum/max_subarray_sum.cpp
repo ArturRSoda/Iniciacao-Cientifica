fcaf4ba7950b40fe002e17cc80566cd0a661c575
root <root@desktop-artur>
2023-09-07 23:14:06 -0300

    Max SubArray Sum

---
+++ Max_Subarray_Sum/max_subarray_sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	int n;
+	cin >> n;
+
+	int array[n];
+	for (int i = 0; i < n; i++) {
+		cin >> array[i];
+	}
+
+	int best = 0;
+	int sum = 0;
+
+	for (int k = 0; k < n; k++) {
+		sum = max(array[k], sum+array[k]);
+		best = max(best, sum);
+	}
+
+	cout << best << "\n";
+
+	return 0;
+}
